f27774bca86c08b3b2416a7851edcc37f2325909
simon <simon@dancingcloudservices.com>
2024-08-07 11:18:25 -0600

    Day 3 Initialization question

---
+++ src/main/java/defaultinter/Ex.java
@@ -0,0 +1,28 @@
+package defaultinter;
+
+interface IntX {
+  default void doStuff() {
+    System.out.println("IntX.doStuff()");
+  }
+}
+
+interface IntY {
+  public abstract void doStuff();
+}
+
+class ClQ implements IntX, IntY {
+  public static void main(String[] args) {
+    new ClQ().doTheStuff();
+  }
+
+  void doStuff() {
+    IntX.super.doStuff();
+  }
+
+  public void doTheStuff() {
+    doStuff(); // line n1
+  }
+}
+
+public class Ex {
+}
